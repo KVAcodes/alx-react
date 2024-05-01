18ec24b0fe680003c1b709b531fdc6fc679e6cbd
kvacodes <kadiriavictor@gmail.com>
2024-05-01 11:22:35 +0100

    added new stuff

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,5 @@
 import $ from 'jquery';
 
-const pgh1 = $('<p>').text('Holberton Dashboard');
-const pgh2 = $('<p>').text('Dashboard data for the students');
-const pgh3 = $('<p>').text('Copyright - Holberton School');
-
-$('body').append(pgh1, pgh2, pgh3);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
