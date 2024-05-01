5b102f26e0d7fea93ddc616931c5b3b90569f3b1
kvacodes <kadiriavictor@gmail.com>
2024-05-01 14:16:09 +0100

    added new stuff

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -16,9 +16,39 @@ module.exports = {
                 use: ["style-loader", "css-loader"]
             },
             {
-                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-                type: 'asset/resource',
-            },
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: '[name].[ext]',
+                      outputPath: 'images',
+                    },
+                  },
+                  {
+                    loader: 'image-webpack-loader',
+                    options: {
+                      mozjpeg: {
+                        progressive: true,
+                        quality: 65
+                      },
+                      optipng: {
+                        enabled: false,
+                      },
+                      pngquant: {
+                        quality: [0.65, 0.90],
+                        speed: 4
+                      },
+                      gifsicle: {
+                        interlaced: false,
+                      },
+                      webp: {
+                        quality: 75
+                      },
+                    },
+                  },
+                ],
+              },         
         ]
     }
 };
